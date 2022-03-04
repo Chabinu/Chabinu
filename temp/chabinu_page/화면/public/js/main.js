a91f4eb613eaab7a9dd939a9e03806a397b11151
KiteJun92 <trust9211@naver.com>
2022-03-04 14:17:51 +0900

    2022-03-04 사진촬영 + 서명 건들기 시작

--- temp/chabinu_page/화면/public/js/main.js
+++ temp/chabinu_page/화면/public/js/main.js
@@ -1,8 +1,32 @@
 
-
+// 헤더 토글
+// navbar JS
 const toggleBtn = document.querySelector(".navbar__toggleBtn");
 const menu = document.querySelector('.navbar__menu');
 
 toggleBtn.addEventListener("click", function(){
   menu.classList.toggle("active");
+});
+
+
+
+
+// 사진 슬라이드
+// Swiper JS
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+    type : 'bullets',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
 });
