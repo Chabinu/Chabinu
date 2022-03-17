6110220fed0d01f09d7d7a3637b4a0b4fd9dd1bb
KiteJun92 <trust9211@naver.com>
2022-03-17 17:46:27 +0900

    2022-03-17 쓸데없는거 지움

--- routes/managers/managerLogin.js
+++ routes/managers/managerLogin.js
@@ -19,29 +19,6 @@ router.post('/',function(req, res, next) {
 });
 
 
-// passport.use(new LocalStrategy({
-//   usernameField: 'managerId',
-//   passwordField: 'managerPw',
-//   session: true,
-//   passReqToCallback: false,
-// }, function (inputId, inputPw, done) {
-  
-//   console.log(inputId, inputPw);
-
-//   connection.query('SELECT ')
-
-//   db.collection('login').findOne({ id: inputId }, function (에러, 결과) {
-//     if (에러) return done(에러)
-
-//     if (!결과) return done(null, false, { message: '존재하지않는 아이디요' })
-//     if (inputPw == 결과.pw) {
-//       return done(null, 결과)
-//     } else {
-//       return done(null, false, { message: '비번틀렸어요' })
-//     }
-//   })
-// }));
-
 
 
 
