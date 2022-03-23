d14b995aaa4fd83748b07b5afab663237022c60c
DESKTOP-L00EK8F\user <trust9211@naver.com>
2022-03-24 00:34:07 +0900

    22-03-24 multer 예제 추가 app.js 수정

--- app.js
+++ app.js
@@ -1,64 +1,130 @@
-
-
-// 모듈 정의 --------------------------------------------------------------------------
-const createError = require('http-errors');
 const express = require('express');
-const path = require('path');
+const morgan = require('morgan');
 const cookieParser = require('cookie-parser');
-const logger = require('morgan');
-const mysql = require('mysql');
+const session = require('express-session');
+const dotenv = require('dotenv');
+const path = require('path');
 
-
-
-// 미들웨어 변수로 정의 --------------------------------------------------------------------------
-const managerRouter = require('./routes/managers/index');
+dotenv.config();
 
 const app = express();
 
+app.set('port', process.env.PORT || 3000);
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
-
-app.use(logger('dev'));
+// 미들웨어
+app.use(morgan('dev'));
+app.use('/', express.static(path.join(__dirname, 'public')));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.urlencoded({extended : false}));
+app.use(cookieParser(process.env.COOKIE_SECRET));
+app.use(session({
+  resave : false,
+  saveUninitialized : false,
+  secret : process.env.COOKIE_SECRET,
+  cookie : {
+    httpOnly : true,
+    secure : false,
+  },
+  name : 'session-cookie',
+}));
 
 
-// 미들웨어 사용 (자바 Servlet 느낌 / 요청주소에 따른 라우터(컨트롤러) 호출) --------------------------------------------------------------------------
+// 멀터 미들웨어
 
-/* 매니저 컴포넌트 */
-app.use('/', managerRouter);
+const multer = require('multer');
+const fs = require('fs');
 
+try{
+  fs.readdirSync('uploads');
+}catch(error){
+  console.error('uploads 폴더가 없어 생성합니다.');
+  fs.mkdirSync('uploads');
+}
 
+const upload = multer({
+  storage : multer.diskStorage({
+    destination(req, file, done){
+      done(null, 'uploads/');
+    },
+    // 저장 파일 명 (originalname + 차량번호 + 시간 + 확장자로 만들기)
+    filename(req, file, done){
+      const ext = path.extname(file.originalname);
+      done(null, path.basename(file.originalname, ext) + '_carNum' + getCurrentDate() + ext);
+      console.log('파일명' + path.basename(file.originalname, ext));
+      console.log('시간' + Date.now());
+    },
+  }),
+  // 용량 제한 10mb
+  limits : {fileSize : 10 * 1024 * 1024},
+})
 
+// 현재 시간 구하는 함수
+function getCurrentDate(){
+  let date = new Date();
+  
+  let year = date.getFullYear.toString();
+  
+  let month = date.getMonth + 1;
+  month = month < 10 ? '0' + month.toString() : month.toString();
 
+  let day = date.getDate();
+  day = day < 10 ? '0' + day.toString() : day.toString();
+  
+  let hour = date.getHours();
+  hour = hour < 10 ? '0' + hour.toString() : hour.toString();
 
+  let minutes = date.getMinutes();
+  minutes = minutes < 10 ? '0' + minutes.toString() : minutes.toString();
 
+  let seconds = date.getSeconds();
+  seconds = seconds < 10 ? '0' + seconds.toString() : seconds.toString();
 
+  return year + month + day + hour + minutes + seconds;
+}
 
-
-
-
-
-// 에러 페이지 관련 --------------------------------------------------------------------------
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+app.get('/upload', (req, res) => {
+  res.sendFile(path.join(__dirname, '/views/multerTest.html'));
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+app.post('/upload', 
+  upload.fields([{ name : 'image1'} , { name : 'image2'} , { name : 'image3'} , { name : 'image4'}]),
+  (req, res) => {
+    console.log(req.files, req.body);
+    res.send('ok');
+  }
+);
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+
+app.use((req, res, next) => {
+  console.log('모든 요청에 실행');
+  next();
 });
 
-module.exports = app;
+app.get('/', (req, res, next) => {
+  // res.sendFile(path.join(__dirname, '/views/mainPage.html'));
+  console.log('GET / 요청 실행');
+  next();
+}, (req, res) => {
+  throw new Error('에러는 에러 처리 미들웨어로 이동');
+});
+
+// app.get('/photo', (req, res, next) => {
+//   res.sendFile(path.join(__dirname, '/views/workDetail.html'));
+//   console.log('GET / 요청 실행');
+//   next();
+// }, (req, res) => {
+//   throw new Error('에러는 에러 처리 미들웨어로 이동');
+// });
+
+app.use((err, req, res, next) => {
+  console.error(err);
+  res.status(500).send(err.message);
+});
+
+app.listen(app.get('port'), ()=>{
+  console.log(app.get('port'), '번 포트에서 준비 중');
+});
+
+
+
+
